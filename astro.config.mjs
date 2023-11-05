e62daabf86bc2f7a34da152e844c7380a31accce
jdwilkin4 <jwilkin4@hotmail.com>
2023-11-05 08:55:06 -0800

    feat: adding readtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://from-mozart-to-javascript.dev/",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
